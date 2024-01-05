42719958dd6665c1367921d77bd24967c351172e
Gurpreet Singh <gursingh7@publicisgroupe.net>
2024-01-05 08:57:12 +0100

    added schema org contract

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,9 @@
-const ImageShortCode = require("./11ty/responsiveImage");
-
 module.exports = function (eleventyConfig) {
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
   eleventyConfig.addPassthroughCopy("src/assets/images");
   eleventyConfig.addPassthroughCopy({
     "./src/site.webmanifest": "site.webmanifest",
   });
-
-  eleventyConfig.addNunjucksAsyncShortcode("responsiveImage", ImageShortCode);
 
   return {
     dir: {
